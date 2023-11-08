d556fe669476f16d45b9afa34464dec2c22918d0
Jean Mercat <jean.mercat@tri.global>
2023-11-07 18:21:50 -0800

    Implement kv cache. The model outputs past_key_values regardless of use_cache (returns None if not used) thus it now returns 3 output values instead of 2 optional inputs past_key_values and use_cache are available in the model and all the way to the attention computation positional embedding takes two extra offset arguments to offset the queries or keys differently. This is useful to offset the query to the end of the kv cache sequence head rotary is not compatible with offsets but will run anyway since it doesn't need to offset anything

--- open_lm/train.py
+++ open_lm/train.py
@@ -163,7 +163,7 @@ def train_one_epoch(model, data, loss, epoch, step, optimizer, scaler, scheduler
         if args.accum_freq == 1:
             with autocast():
                 inputs, targets = sample_chunk(texts, args)
-                out, _ = model(inputs)
+                out, _, _ = model(inputs)
 
                 if args.log_logit_mean:
                     logit_m.update(torch.mean(out).item())
@@ -190,7 +190,7 @@ def train_one_epoch(model, data, loss, epoch, step, optimizer, scaler, scheduler
                         if inputs_ii.shape[0] == 0:
                             break
                         targets_ii = targets[ii * per_batch : (ii + 1) * per_batch]
-                        out, _ = model(inputs_ii)
+                        out, _, _ = model(inputs_ii)
 
                         if args.log_logit_mean:
                             logit_m.update(torch.mean(out).item())
@@ -320,7 +320,7 @@ def evaluate(model, data, start_epoch, args, writer):
         with autocast():
             inputs, targets = sample_chunk(texts, args)
 
-            out, _ = model(inputs)
+            out, _, _ = model(inputs)
             total_loss = loss(out.reshape(-1, args.vocab_size), targets.reshape(-1))
             losses_m.update(total_loss.item(), inputs.shape[0])
         batch_time_m.update(time.time() - end)
